9844b63fb2e2e8d1099ad6b3ecc63c09e5a75707
jeffreyhorner <jeffrey.horner@gmail.com>
2010-09-15 16:52:18 -0500

    Fixed NAMESPACE and added preliminary support for apr

--- src/httpd.c
+++ src/httpd.c
@@ -1,3 +1,18 @@
+/*
+ * Apache Portable Runtime
+ */
+#include <apr.h>
+#include <apr_network_io.h>
+#include <apr_errno.h>
+#include <apr_strings.h>
+#include <apr_env.h>
+#include <apr_pools.h>
+#include <apr_hash.h>
+#include <apr_thread_mutex.h>
+
+/*
+ * R headers
+ */
 #include <R.h>
 #include <Rinternals.h>
 #include <R_ext/Rdynload.h>
@@ -10,9 +25,10 @@ R_CallMethodDef callMethods[]  = {
 	{NULL, NULL, 0}
 };
 
-void R_init_mylib(DllInfo *info) {
+void R_init_httpd(DllInfo *info) {
 	R_registerRoutines(info, NULL, callMethods, NULL, NULL);
+	R_useDynamicSymbols(info, FALSE);
 }
      
-void R_unload_mylib(DllInfo *info) {
+void R_unload_httpd(DllInfo *info) {
 }
